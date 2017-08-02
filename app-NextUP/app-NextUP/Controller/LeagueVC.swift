a6ce9d6a7e122667db1e7943d68b607662b120f4
LouisCollinsJr <louiscollinsjr@marginal.space>
2017-08-02 13:47:01 -0500

    buttons disabled

--- app-NextUP/app-NextUP/Controller/LeagueVC.swift
+++ app-NextUP/app-NextUP/Controller/LeagueVC.swift
@@ -9,20 +9,46 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
-   
+    @IBOutlet weak var nextBtn: BorderButton!
+    var player: Player!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+       player = Player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
        performSegue(withIdentifier: "skillVCSegue", sender: self)
     }
     
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+        nextBtn.isEnabled = true
+    
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType:"womens")
+    
+    }
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeague(leagueType:"coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+        nextBtn.backgroundColor = UIColor.gray
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? skillVC {
 
-   /*
+        }
+    }
+    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
---
+++ app-NextUP/app-NextUP/Model/Player.swift
@@ -0,0 +1,13 @@
+//
+//  Player.swift
+//  app-NextUP
+//
+//  Created by Larry Louis Collins Jr on 8/2/17.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var desiredSkillLevel: String?
+}
